63ffeb47f2e5aaac3ec91837e066ffd7529cfc4a
arsyds <arsyds@proton.me>
2024-06-03 13:01:54 +0700

    feat: project initialize

---
+++ views/site/user.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'User';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-user">
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eveniet impedit quidem nobis laboriosam cupiditate dolor earum eos dicta, totam obcaecati ut temporibus laudantium, cum, odio provident velit? Voluptas, sequi.
+    </p>
+</div>
